eb6c78a68cf21f3a85adcb8e8e776124ffae65e5
levi hicks <lv.hicks@gmail.com>
2022-04-29 23:42:51 -0500

    add documentation, cooldown period, and limit one pixel per transaction

--- contracts/Decentrawall.sol
+++ contracts/Decentrawall.sol
@@ -1,23 +1,33 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8;
 
+/** @title A decentralized wall users paint on
+ *  @author Levi Hicks
+ */
 contract Decentrawall {
     bytes[2000][2000] public wall;
+    mapping(address => uint) public cooldownExpirations;
 
     struct WallPixel {
-        uint x;
-        uint y;
+        uint row;
+        uint col;
         uint r;
         uint g;
         uint b;
     }
 
-    function set(WallPixel[] memory wp) public {
-        require(wp.length <= 2000, "Invalid input array length.");
-        for (uint i = 0; i < wp.length; i++) {
-            require(wp[i].x < 2000 && wp[i].y < 2000, "Invalid coordinates.");
-            require(wp[i].r <= 255 && wp[i].g <= 255 && wp[i].b <= 255, "Invalid color input.");
-            wall[wp[i].x][wp[i].y] = abi.encodePacked(wp[i].r, wp[i].g, wp[i].b);
-        }
+    /** @dev paints a pixel on the decentrawall
+     *  @notice there is a cooldown period of one hour until you can paint a new pixel
+     *  @param wp contains row, column, and RGB values for pixel to paint
+     */
+    function paintPixel(WallPixel memory wp) public {
+        require(wp.row < 2000 && wp.col < 2000, "Invalid coordinates.");
+        require(wp.r <= 255 && wp.g <= 255 && wp.b <= 255, "Invalid color input.");
+        require(
+            cooldownExpirations[msg.sender] == 0 || cooldownExpirations[msg.sender] <= block.timestamp,
+            "Cooldown period not yet passed."
+        );
+        wall[wp.row][wp.col] = abi.encodePacked(wp.r, wp.g, wp.b);
+        cooldownExpirations[msg.sender] = block.timestamp + 1 hours;
     }
 }
